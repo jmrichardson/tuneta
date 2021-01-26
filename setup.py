1c2536dd4d933dc58041669afbcb4b6bcf3423d8
John Richardson <jmrichardson@gmail.com>
2021-01-26 17:23:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tuneta",
-    version="0.1.13",
+    version="0.1.14",
     author="John Richardson",
     author_email="jmrichardson@gmail.com",
     description="Optimize financial technical indicators for machine learning",
